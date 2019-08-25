0849e2828edaa33966c3d96c7166b11018125082
Enishi Naka <enaka1223@gmail.com>
2019-08-25 21:00:17 +0900

    Install react-navigation

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
 import { Font } from 'expo-font';
 import fontAwesome from '../../assets/fonts/fa-solid-900.ttf';
 import { createIconSet } from 'react-native-vector-icons';
@@ -23,7 +23,7 @@ class CircleButton extends React.Component {
   }
 
   render() {
-    const { name, style, color } = this.props;
+    const { name, style, color, onPress } = this.props;
 
     let bgColor = '#E31676';
     let textColor = '#fff';
@@ -34,21 +34,27 @@ class CircleButton extends React.Component {
     }
 
     return (
-      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
-        {
-          this.state.fontLoaded ? (
-            <CustomIcon name={name} style={[styles.circleButtonTitle, { color: textColor }]} />
-          ) : null
-        }
-      </View>
+      <TouchableHighlight style={[styles.container, style]} onPress={onPress} underlayColor="transparent">
+        <View style={[styles.circleButton, { backgroundColor: bgColor }]}>
+          {
+            this.state.fontLoaded ? (
+              <CustomIcon name={name} style={[styles.circleButtonTitle, { color: textColor }]} />
+            ) : null
+          }
+        </View>
+      </TouchableHighlight>
     );
   }
 }
 const styles = StyleSheet.create({
-  circleButton: {
+  container: {
+    width: 48,
+    height: 48,
     position: 'absolute',
     bottom: 32,
     right: 32,
+  },
+  circleButton: {
     width: 48,
     height: 48,
     borderRadius: 24,
