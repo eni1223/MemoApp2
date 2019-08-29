c796f9805eac3a2a748075883358c82944687c5c
Enishi Naka <enaka1223@gmail.com>
2019-08-29 16:49:31 +0900

    MemoEditScreen body error

--- src/screens/MemoEditScreen.js
+++ src/screens/MemoEditScreen.js
@@ -15,20 +15,29 @@ class MemoEditScreen extends React.Component {
     const { params } = this.props.navigation.state;
     this.setState({
       body: params.memo.body,
-      key: params.memo.key
+      key: params.memo.key,
      });
   }
 
   handlePress() {
     const { currentUser } = firebase.auth();
     const db = firebase.firestore();
+    const newDate = firebase.firestore.Timestamp.now();
     console.log(this.state);
     db.collection(`users/${currentUser.uid}/memos`).doc(this.state.key)
       .update({
         body: this.state.body,
+        createdOn: newDate,
       })
       .then(() => {
-        console.log('success!');
+        const { navigation } = this.props;
+        navigation.state.params.returnMemo({
+          body: this.state.body,
+          key: this.state.key,
+          createdOn: this.state.createdOn,
+          createdOn: newDate,
+        });
+        navigation.goBack();
       })
       .catch((error) => {
         console.log(error);
